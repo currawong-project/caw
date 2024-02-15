ff643dd314e19007c6050738315fa3a5efd83adc
kevin <kevin@larke.org>
2024-02-15 16:30:54 -0500

    Added cfg. file parsing.

--- src/proj/main.cpp
+++ src/proj/main.cpp
@@ -1,6 +1,10 @@
 #include "cwCommon.h"
 #include "cwLog.h"
 #include "cwCommonImpl.h"
+#include "cwText.h"
+#include "cwObject.h"
+
+using namespace cw;
 
 void print( void* arg, const char* text )
 {
@@ -9,10 +13,37 @@ void print( void* arg, const char* text )
 
 int main( int argc, char* argv[] )
 {
+  rc_t rc = kOkRC;
+  object_t* cfg = nullptr;
   cw::log::createGlobal();
   
-  cwLogInfo("Project template");
+  cwLogInfo("Project template: args:%i", argc);
 
+  if( argc < 2 || textLength(argv[1])==0 )
+  {
+    cwLogError(kInvalidArgRC,"No cfg. file was given.");
+    goto errLabel;
+  }
+  else
+  {
+    const char* val = nullptr;
+    
+    if((rc = objectFromFile(argv[1],cfg)) != kOkRC )
+    {
+      cwLogError(rc,"The file '%s'.",argv[1]);
+      goto errLabel;
+    }
+
+    if((rc = cfg->getv("param",val)) != kOkRC )
+    {
+      cwLogError(kSyntaxErrorRC,"The 'param' cfg. field was not found.");
+      goto errLabel;
+    }
+
+    cwLogInfo("param=%s",cwStringNullGuard(val));
+  }
+
+errLabel:
   cw::log::destroyGlobal();
 
   return 0;
