e18288f7cc847c276873c6b96024ca1bcab23e42
kevin <kevin@larke.org>
2024-06-10 16:29:17 -0400

    main.cpp,io.cfg,main.cfg,caw_0.cfg : Initial working real-time version.

--- src/caw/main.cpp
+++ src/caw/main.cpp
@@ -6,12 +6,10 @@
 #include "cwText.h"
 #include "cwObject.h"
 #include "cwFileSys.h"
-#include "cwTime.h"
-#include "cwMidiDecls.h"
-#include "cwFlowDecl.h"
-#include "cwFlow.h"
-
 #include "cwIo.h"
+
+#include "cwIoFlowCtl.h"
+
 
 
 using namespace cw;
@@ -20,20 +18,15 @@ rc_t test( const object_t* cfg, int argc, char* argv[] );
 
 typedef struct app_str
 {
-  object_t* cfg;           // complete cfg.
+  object_t*             cfg;                // complete cfg.
   
-  bool            real_time_fl;  // Execute in non-real-time mode
-  const char*     pgm_label;     // 
-  const object_t* pgm_cfg;       //
-  const char*     base_dir;      //  
-  char*           proj_dir;      //  Project directory <base_dir>/<pgm_label>
-  object_t*       io_cfg;        //  IO lib cfg.
-  object_t*       proc_class_dict_cfg; //
-  object_t*       subnet_dict_cfg; 
+  object_t*             flow_cfg;           // flow program cfg
+  object_t*             io_cfg;             //  IO lib cfg.
+  const char*           cmd_line_pgm_label; // 
   
-  unsigned        value;
-  io::handle_t    ioH;
-  
+  io::handle_t          ioH;
+  io_flow_ctl::handle_t ioFlowH;
+
 } app_t;
 
 enum
@@ -65,6 +58,36 @@ void print( void* arg, const char* text )
   printf("%s\n",text);
 }
 
+rc_t _load_init_pgm( app_t& app, const char* pgm_label, bool& exec_complete_fl_ref )
+{
+  rc_t     rc = kOkRC;
+  unsigned pgm_idx;
+
+  exec_complete_fl_ref = false;
+  
+  if((pgm_idx = program_index(app.ioFlowH,pgm_label)) == kInvalidIdx )
+  {
+    rc = cwLogError(kInvalidArgRC,"A program named '%s' was not be found.",cwStringNullGuard(pgm_label));
+    goto errLabel;
+  }
+
+  if((rc = program_load(app.ioFlowH,pgm_idx)) != kOkRC )
+  {
+    rc = cwLogError(rc,"Program load failed on '%s'.",cwStringNullGuard(pgm_label));
+    goto errLabel;
+  }
+
+  if( is_program_nrt(app.ioFlowH) )
+  {
+    exec_complete_fl_ref = true;
+    if((rc = exec_nrt(app.ioFlowH)) != kOkRC )
+      goto errLabel;
+  }
+  
+
+errLabel:
+  return rc;
+}
 
 rc_t _ui_value_callback(app_t* app, const io::ui_msg_t& m )
 {
@@ -90,8 +113,8 @@ rc_t _ui_value_callback(app_t* app, const io::ui_msg_t& m )
       break;
       
     case kValueNumbId:
-      app->value = m.value->u.u;
-      cwLogInfo("Setting value:%i",app->value);
+      //app->value = m.value->u.u;
+      //cwLogInfo("Setting value:%i",app->value);
       break;
     
   }
@@ -104,7 +127,7 @@ rc_t _ui_echo_callback(app_t* app, const io::ui_msg_t& m )
   {
     case kValueNumbId:
       {
-        uiSendValue( app->ioH, io::uiFindElementUuId( app->ioH, kValueNumbId ), app->value );
+        //uiSendValue( app->ioH, io::uiFindElementUuId( app->ioH, kValueNumbId ), app->value );
       }
       break;
     
@@ -184,6 +207,8 @@ rc_t _io_callback( void* arg, const io::msg_t* m )
       break;
       
     case io::kAudioTId:
+      if( app->ioFlowH.isValid() && m != nullptr )
+        io_flow_ctl::exec(app->ioFlowH,*m);
       break;
       
     case io::kAudioMeterTId:
@@ -209,15 +234,11 @@ rc_t _io_callback( void* arg, const io::msg_t* m )
   return kOkRC;
 }
 
-rc_t _parse_cfg( app_t& app, int argc, char* argv[] )
+rc_t _parse_main_cfg( app_t& app, int argc, char* argv[] )
 {
   rc_t rc = kOkRC;
-  const char* io_cfg_fn = nullptr;
-  const char* mode_label = nullptr;
-  const object_t* pgmL = nullptr;
-  const char* proc_cfg_fname = nullptr;
-  const char* subnet_cfg_fname = nullptr;
-  if( argc < 3 )
+
+  if( argc < 2 )
   {
     cwLogPrint("Usage: caw <cfg_fname> <pgm_label>");
     rc = kInvalidArgRC;
@@ -225,123 +246,58 @@ rc_t _parse_cfg( app_t& app, int argc, char* argv[] )
   }
   else
   {
+    const char* io_cfg_fn = nullptr;
+    const char* flow_cfg_fn = nullptr;
+
+    // check for pgm label
+    if( argc > 2 )
+    {
+      if( textLength(argv[2]) == 0 )
+      {
+        rc = cwLogError(kSyntaxErrorRC,"The command line program label is blank or invalid valid.");
+        goto errLabel;
+      }
+
+      app.cmd_line_pgm_label = argv[2];
+    }
+    
     // parse the cfg. file
     if((rc = objectFromFile(argv[1],app.cfg)) != kOkRC )
     {
       rc = cwLogError(rc,"Parsing failed on the cfg. file '%s'.",argv[1]);
       goto errLabel;
     }
-    
-    // parse the cfg parameters
-    if((rc = app.cfg->readv("param",  0, app.value,
-                            "io_cfg", kOptFl, io_cfg_fn,
-                            "base_dir", 0, app.base_dir,
-                            "proc_dict",0,proc_cfg_fname,
-                            "subnet_dict",0,subnet_cfg_fname,                            
-                            "mode", 0, mode_label,
-                            "programs", kDictTId, pgmL)) != kOkRC )
+
+    // read the cfg file
+    if((rc = app.cfg->readv("flow", kReqFl, flow_cfg_fn,
+                            "io",   kReqFl, io_cfg_fn)) != kOkRC )
     {
-      rc = cwLogError(rc,"'caw' system parameter processing failed.");
+      rc = cwLogError(rc,"Parsing failed on '%s'.",argv[1]);
       goto errLabel;
     }
 
-    // set the real-time mode flag
-    app.real_time_fl = !textIsEqual(mode_label,"non_real_time");
-
-    // if we are in real-time mode then the io_cfg file must be given
-    if( app.real_time_fl && io_cfg_fn == nullptr )
+    // parse the IO cfg file
+    if((rc = objectFromFile(io_cfg_fn,app.io_cfg)) != kOkRC )
     {
-      rc = cwLogError(kSyntaxErrorRC,"To operation in real-time mode an 'io_cfg' file must be provided.");
-      goto errLabel;
-    }
-    
-    // parse the 'io_cfg' file
-    if( app.real_time_fl )
-      if((rc = objectFromFile(io_cfg_fn,app.io_cfg)) != kOkRC )
-      {
-        rc = cwLogError(rc,"'caw' IO cfg. file parsing failed on '%s'.",cwStringNullGuard(io_cfg_fn));
-        goto errLabel;
-      }
-
-
-    // parse the proc dict. file
-    if((rc = objectFromFile(proc_cfg_fname,app.proc_class_dict_cfg)) != kOkRC )
-    {
-      rc = cwLogError(rc,"The flow proc dictionary could not be read from '%s'.",cwStringNullGuard(proc_cfg_fname));
+      rc = cwLogError(rc,"Parsing failed on '%s'.",cwStringNullGuard(io_cfg_fn));
       goto errLabel;
     }
 
-    // parse the subnet dict file
-    if((rc = objectFromFile(subnet_cfg_fname,app.subnet_dict_cfg)) != kOkRC )
+    // parse the flow cfg file
+    if((rc = objectFromFile(flow_cfg_fn,app.flow_cfg)) != kOkRC )
     {
-      rc = cwLogError(rc,"The flow subnet dictionary could not be read from '%s'.",cwStringNullGuard(subnet_cfg_fname));
+      rc = cwLogError(rc,"Parsing failed on '%s'.",cwStringNullGuard(flow_cfg_fn));
       goto errLabel;
     }
-        
-    // get the pgm label
-    if( textLength(argv[2]) == 0 )
-    {
-      rc = cwLogError(kSyntaxErrorRC,"No 'caw' program label was given.");
-      goto errLabel;
-    }
-    
-    app.pgm_label = argv[2];
-
-    // find the parameters for the requested program
-    for(unsigned i=0; i<pgmL->child_count(); i++)      
-    {
-      const object_t* pgm = pgmL->child_ele(i);
-      if( textIsEqual( pgm->pair_label(), app.pgm_label ) )
-      {
-        if( pgm->pair_value() == nullptr || !pgm->pair_value()->is_dict() )
-        {
-          rc = cwLogError(kSyntaxErrorRC,"The parameters for the program '%s' is not a dictionary.",cwStringNullGuard(app.pgm_label));
-          goto errLabel;
-        }
-        
-        app.pgm_cfg = pgm->pair_value();
-        break;
-      }      
-    }
-
-    if( app.pgm_cfg == nullptr )
-      rc =cwLogError(kEleNotFoundRC,"The program '%s' was not found in the cfg. program list.",cwStringNullGuard(app.pgm_label));
-    else
-    {
-      if((app.proj_dir = filesys::makeFn(app.base_dir,nullptr,nullptr,app.pgm_label,nullptr)) == nullptr )
-      {
-        rc = cwLogError(kOpFailRC,"An error occurred while forming the the project directory name for '%s' / '%s'.",cwStringNullGuard(app.base_dir),cwStringNullGuard(app.pgm_label));
-        goto errLabel;
-      }
-
-      if( !filesys::isDir(app.proj_dir))
-      {
-        if((rc = filesys::makeDir(app.proj_dir)) != kOkRC )
-        {
-          rc = cwLogError(kOpFailRC,"Project directory create failed.");
-          goto errLabel;
-        }
-      }
-    }
-
-    
   }
+
 errLabel:
-  if( rc != kOkRC )
-    rc = cwLogError(rc,"App. cfg. parse failed.");
   return rc;
 }
 
-rc_t _main_gui( app_t& app )
+rc_t _io_main( app_t& app )
 {
   rc_t rc = kOkRC;
-  
-  if((rc = create( app.ioH, app.io_cfg, _io_callback, &app, appIdMapA, appIdMapN ) ) != kOkRC )
-  {
-    rc = cwLogError(rc,"IO create failed.");
-    goto errLabel;
-  }
-
   
   // start the IO framework instance
   if((rc = io::start(app.ioH)) != kOkRC )
@@ -351,7 +307,7 @@ rc_t _main_gui( app_t& app )
   }
 
   
-  // execute the io framework
+  // execute the IO framework
   while( !io::isShuttingDown(app.ioH))
   {
     // This call will block on the websocket handle
@@ -373,60 +329,63 @@ errLabel:
 }
 
 
-
 int main( int argc, char* argv[] )
 {
   rc_t  rc  = kOkRC;
   app_t app = {};
-  flow::handle_t flowH;
+  bool exec_complete_fl = false;
   
   cw::log::createGlobal();
-  
-  cwLogInfo("caw: args:%i", argc);
 
-  if((rc = _parse_cfg(app,argc,argv)) != kOkRC )
+  unsigned appIdMapN = sizeof(appIdMapA)/sizeof(appIdMapA[0]);
+
+  if((rc= _parse_main_cfg(app, argc, argv )) != kOkRC )
     goto errLabel;
 
-    // create the flow object
-  if((rc = create( flowH,
-                   app.proc_class_dict_cfg,
-                   app.pgm_cfg,
-                   app.subnet_dict_cfg,
-                   app.proj_dir)) != kOkRC )
+  // instantiate the IO framework
+  if((rc = create( app.ioH, app.io_cfg, _io_callback, &app, appIdMapA, appIdMapN, nullptr )) != kOkRC )
   {
-    //rc = cwLogError(rc,"Flow object create failed.");
+    rc = cwLogError(rc,"IO Framework instantiation failed.");
     goto errLabel;
   }
 
-  // run the network
-  if((rc = exec( flowH )) != kOkRC )
-    rc = cwLogError(rc,"Execution failed.");
-    
+  report(app.ioH);
+
+  // instantiate the IO flow controller
+  if((rc = create( app.ioFlowH, app.ioH, app.flow_cfg)) != kOkRC )
+  {
+    rc = cwLogError(rc,"IO-Flow instantiation failed.");
+    goto errLabel;
+  }
+
+  if( app.cmd_line_pgm_label != nullptr )
+    if((rc = _load_init_pgm(app, app.cmd_line_pgm_label, exec_complete_fl )) != kOkRC )
+      goto errLabel;
+  
+
+  if( !exec_complete_fl )
+    _io_main(app);
+
+
 errLabel:
-  // destroy the flow object
-  if((rc = destroy(flowH)) != kOkRC )
-  {
-    rc = cwLogError(rc,"Close the flow object.");
-    goto errLabel;
-  }
-
-  destroy(app.ioH);
+  if((rc = destroy(app.ioFlowH)) != kOkRC )
+    rc = cwLogError(rc,"IO Flow destroy failed.");
   
-  mem::release(app.proj_dir);
+  if((rc = destroy(app.ioH)) != kOkRC )
+    rc = cwLogError(rc,"IO destroy failed.");
 
-  if( app.proc_class_dict_cfg != nullptr )
-    app.proc_class_dict_cfg->free();
-
-  if( app.subnet_dict_cfg != nullptr )
-    app.subnet_dict_cfg->free();
-  
   if( app.io_cfg != nullptr )
     app.io_cfg->free();
   
+  if( app.flow_cfg != nullptr )
+    app.flow_cfg->free();
+
   if( app.cfg != nullptr )
     app.cfg->free();
-  
-  cw::log::destroyGlobal();
 
-  return 0;
+  cw::log::destroyGlobal();
+  
+  return rc;
 }
+
+
