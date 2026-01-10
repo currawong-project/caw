2391dd6377ab8c4be6451b5c99c22e49f8bc4e7d
kevin <kevin@larke.org>
2026-01-09 21:57:48 -0500

    Removed automake related build files.

--- build/linux/debug/vgrind.sh
+++
@@ -1,5 +0,0 @@
-# export LD_LIBRARY_PATH=~/sdk/libwebsockets/build/out/lib
-# valgrind --leak-check=yes --log-file=vg0.txt --track-origins=yes bin/caw ui ~/src/caw/src/caw/ui/ui_caw.cfg $1
-# valgrind --leak-check=yes --log-file=vg0.txt --track-origins=yes bin/caw test ~/src/cwtest/src/cwtest/cfg/main.cfg /flow_value all echo
-valgrind --leak-check=yes --log-file=vg0.txt --track-origins=yes bin/caw ui ~/src/caw/src/caw/perf/perf_caw.cfg
-
